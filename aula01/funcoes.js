efc790535a26153fbd95e06a27d0103f68e887e9
Dimitry <sibulskis.dimitry@hotmail.com>
2024-04-03 08:36:12 -0300

    Aula 2 sobre funções e métodos

--- aula01/funcoes.js
+++ aula01/funcoes.js
@@ -3,6 +3,7 @@ const num2 = 3
 var msg = "Olá amigos! Esse é o número:"
 
 console.log(msg , num1)
+console.log(msg + num1)
 console.log(num1 + num2)
 console.log(typeof num1)
 console.log(typeof msg)
