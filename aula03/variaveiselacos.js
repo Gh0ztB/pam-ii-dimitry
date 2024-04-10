6c760296770d3c803f61f1013cdaffb16023c111
Dimitry Sibulskis <sibulskis.dimitry@hotmail.com>
2024-04-10 08:33:57 -0300

    Aula 03 sobre arrays e objetos

---
+++ aula03/variaveiselacos.js
@@ -0,0 +1,24 @@
+let n = 2
+let t = "oi"
+
+console.log(typeof n)
+console.log(typeof t)
+console.log(typeof t, typeof n)
+
+let a = [0,1,1,2,3,5,8,13,21]
+
+console.log(typeof a)
+
+let o = {nome: "joão", idade: "36"} //JSON
+//JavaScript Object Notation
+console.log(typeof o)
+
+console.log(a)
+console.log(a[6])
+
+a.forEach((el) =>
+    console.log(`Elemento:  ${el}`)
+)
+
+console.log(o)
+console.log(o.nome, "tem", o.idade)
